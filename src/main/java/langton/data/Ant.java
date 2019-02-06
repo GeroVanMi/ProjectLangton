c35a409ce1b2256b72a989c07c8e5ebc0d3cf561
gerome <gerome.meyer@icloud.com>
2019-02-06 16:32:54 +0100

    Creation of Ant class + simple helper classes

--- src/main/java/langton/data/Ant.java
+++ src/main/java/langton/data/Ant.java
@@ -1,4 +1,44 @@
 package langton.data;
 
+import langton.helpers.Direction;
+import langton.helpers.Point;
+
 public class Ant {
+
+    private Point position;
+    private Direction direction;
+
+    public Ant(Point position, Direction direction) {
+        this.position = position;
+        this.direction = direction;
+    }
+
+    public void tick() {
+        this.move();
+    }
+
+    public void move() {
+        switch (direction) {
+            case UP:
+                position.decreaseY();
+                break;
+            case RIGHT:
+                position.increaseX();
+                break;
+            case DOWN:
+                position.increaseY();
+                break;
+            case LEFT:
+                position.decreaseX();
+                break;
+        }
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
 }
--- src/main/java/langton/helpers/Point.java
+++ src/main/java/langton/helpers/Point.java
@@ -1,4 +1,34 @@
 package langton.helpers;
 
 public class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void increaseX() {
+        this.x++;
+    }
+
+    public void decreaseX() {
+        this.x--;
+    }
+
+    public void increaseY() {
+        this.y--;
+    }
+
+    public void decreaseY() {
+        this.y--;
+    }
 }
