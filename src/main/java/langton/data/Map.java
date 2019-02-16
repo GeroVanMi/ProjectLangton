b301e1af0ee6548ac737050af8a73f8d55acd035
Natalie Breu <nabreu@bluewin.ch>
2019-02-16 15:46:42 +0100

    Add JavaDocs at Field and Map

--- src/main/java/langton/data/Map.java
+++ src/main/java/langton/data/Map.java
@@ -5,16 +5,29 @@ import javafx.scene.paint.Color;
 /**
  * @author Natalie Breu
  * @version 14_02_2019
- *
+ * <p>
+ *  This class handel the creation and increment of the field, on which the ant(s) move.
  * TODO: Add JavaDoc
  */
 public class Map {
 
     private Field[][] fields;
 
+    /**
+     * @param rows
+     * @param columns
+     */
+
     public Map(int rows, int columns) {
         fields = new Field[rows][columns];
     }
+
+    /**
+     * When the ant reaches the end of the field, this method increases the field.
+     *
+     * @param amountX The initial amount of fields which increases the rows.
+     * @param amountY The initial amount of fields which increases the columns.
+     */
 
     public void increaseMapSize(int amountX, int amountY) {
         for (int x = 0; x < amountX; x++) {
@@ -24,6 +37,10 @@ public class Map {
         }
     }
 
+    /**
+     * This method created the startfield with a lenght and a width of 50 grey fields.
+     */
+
     public void generateMap() {
         for (int x = 0; x < fields.length; x++) {
             for (int y = 0; y < fields[x].length; y++) {
@@ -32,19 +49,24 @@ public class Map {
         }
     }
 
+    /**
+     * @return Returns the fields.
+     */
     public Field[][] getFields() {
         return fields;
     }
 
+    /**
+     * @return Returns the amount of the rows.
+     */
     public int getRowsCount() {
         return fields.length;
     }
 
+    /**
+     * @return Returns the amount of the coloms.
+     */
     public int getColumnsCount() {
         return fields[0].length;
-    }
-
-    public void setFields(Field[][] fields) {
-        this.fields = fields;
     }
 }
